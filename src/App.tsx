41ac52dad4f11070db81a8a9f52b8fc065456179
Shamir Leon <leonshamir1@gmail.com>
2024-08-21 21:37:10 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <div className="font-Nunito">
+      <p className="underline">Countries</p>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: '#2B3945',         // Dark Blue for Dark Mode Elements
+        veryDarkBlue: '#202C37',     // Very Dark Blue for Dark Mode Background
+        deepBlueBlack: '#111517',    // Very Dark Blue for Light Mode Text
+        mediumGray: '#858585',       // Dark Gray for Light Mode Input
+        lightGray: '#FAFAFA',        // Very Light Gray for Light Mode Background
+        pureWhite: '#FFFFFF',        // White for Dark Mode Text & Light Mode Elements
+      },
+      fontFamily: {
+        'Nunito': ['"Nunito Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
