a72d94da6771d48cceb82a7580875d39e28f9971
Shamir Leon <leonshamir1@gmail.com>
2024-08-22 18:15:59 -0500

    Feat: add types, some states and functions

--- src/context/CountriesContext.tsx
+++ src/context/CountriesContext.tsx
@@ -1,3 +1,4 @@
 import { createContext } from "react";
+import { IContext } from "../interfaces/interfaces";
 
-export const CountriesContext = createContext({});
+export const CountriesContext = createContext({} as IContext);
--- src/context/CountriesProvider.tsx
+++ src/context/CountriesProvider.tsx
@@ -1,9 +1,36 @@
 import { CountriesContext } from "./CountriesContext";
+import useGetCountries from "../hooks/useGetCountries";
+import { useState } from "react";
+// import { useEffect } from "react";
 
 export default function CountriesProvider({ children } : { children: React.ReactNode }) {
-    console.log("CountriesProvider");
+    const [darkMode, setDarkMode] = useState(false);
+    const { countries, loading, error } = useGetCountries();
+
+    const localDarkMode = JSON.parse(localStorage.getItem("darkMode") || "false");
+    
+    if (localDarkMode) {
+        document.body.classList.add("dark");
+    }
+
+    /* FUNCTIONS */
+    const toggleDarkMode = () => {
+        document.body.classList.toggle("dark");
+        localStorage.setItem("darkMode", JSON.stringify(darkMode));
+        setDarkMode(!darkMode);
+    }
+
+
     return (
-        <CountriesContext.Provider value={{}}>
+        <CountriesContext.Provider value={{
+            /* States */
+            countries,
+            darkMode,
+            loading,
+            error,
+            /* Functions */
+            toggleDarkMode
+        }}>
             {children}
         </CountriesContext.Provider>
     )
